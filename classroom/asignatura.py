be7e038a345b02085f850fc916148f75ef4d2c33
Juan Camilo Gutiérrez Martínez <juagutierrezma@unal.edu.co>
2023-03-05 19:28:08 -0500

    Intento_1

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,10 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    #Simulacion sobrecarga constructores.
+    def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+         impresion=self._nombre+" "+self._salon
+         return impresion
